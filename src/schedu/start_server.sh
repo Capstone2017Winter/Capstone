fb043ea27e062d55a58a4eefc39f1daf0c32e7bd
root <root@ip-172-31-18-73.us-west-2.compute.internal>
2017-01-31 20:05:55 -0700

    server set up and gitignore

---
+++ src/schedu/start_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+APPNAME=schedu
+APPDIR=/home/ubuntu/Capstone/src/$APPNAME/
+
+LOGFILE=$APPDIR'gunicorn.log'
+ERRORFILE=$APPDIR'gunicorn-error.log'
+
+NUM_WORKERS=3
+
+ADDRESS=127.0.0.1:8000
+
+cd $APPDIR
+
+exec gunicorn $APPNAME.wsgi:application \
+-w $NUM_WORKERS --bind=$ADDRESS \
+--log-level=debug \
+--log-file=$LOGFILE 2>>$LOGFILE 1>>$ERRORFILE &
+
