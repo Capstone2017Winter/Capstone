bf86d44521e68a63242ef7661791b0566461ffc8
edcarter <edcarter@ualberta.ca>
2017-02-04 17:59:03 -0700

    update models

--- src/schedu/schedule_builder/models.py
+++ src/schedu/schedule_builder/models.py
@@ -4,8 +4,60 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+class User(models.Model)
+	degree = models.ForeignKey(Degree)
+	name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.name
+
+class Schedule(models.Model)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	creation_date = models.DateTimeField('date created', auto_now_add=True)
+	modified_date = models.DateTimeField('date modified', auto_now=True)
+	year = models.CharField(max_length=10) #eg 2017
+	term = models.CharField(max_length=10) #eg FALL
+
+	def __str__(self):
+		return 'user:{}, year:{}, term:{}'.format(self.user, self.year, self.term)
+
+class ClassSection(models.Model)
+	schedule = models.ManyToManyField(Schedule)
+	myclass = models.ForeignKey(MyClass, on_delte=models.CASCADE)
+	section_number = models.CharField(max_length=10) #eg EB2
+
+	def __str__(self):
+		return 'class:{}, section:{}'.format(self.myclass, self.section_number)
+
+class TimeSlot
+	class_section = models.ManyToManyField(ClassSection)
+	time = models.TimeField()
+	day = models.CharField(max_length=20)
+	duration = models.DurationField()
+
+	def __str__(self):
+		return 'time:{}, day:{}, duration:{}'.format(self.time, self.day, self.duration)
+
+class Degree
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return self.name
+
+class MyClass
+	degree = models.ManyToManyField(Degree)
+	class_code = models.CharField(max_length=10, unique=True)
+	class_description = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.class_code
+
+class ScheduleImage(models.Model)
+	schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE) #warning, if image is stored on file system, it must be manually deleted
+	creation_date = models.DateTimeField('date created', auto_now_add=True)
+
 class Image(models.Model):
 	image = models.ImageField('img', upload_to='path/')
 
 	def __str__(self):
-		return image.name
+		return self.image.name
