8ef9136356a95bbec926bb7a43a8d855ff78c5e2
Salim1993 <salimbenkhaled93@gmail.com>
2017-02-05 16:36:02 -0700

    Have the C Files set up but wont compile since it assuming Im using Flash memory. Either implement Flash or a way to get around it

---
+++ hardware/niosII_microc_capstone/software/capstone/obj/default/dm9000a.d
@@ -0,0 +1,124 @@
+obj/default/dm9000a.o: dm9000a.c dm9000a.h \
+  ../capstone_bsp//iniche/inc/alt_iniche_dev.h \
+  ../capstone_bsp//iniche/src/h/nios2/ipport.h ../capstone_bsp/system.h \
+  ../capstone_bsp/linker.h ../capstone_bsp//iniche/src/h/nptypes.h \
+  ../capstone_bsp//HAL/inc/includes.h ../capstone_bsp//HAL/inc/os_cpu.h \
+  ../capstone_bsp//HAL/inc/sys/alt_irq.h ../capstone_bsp//HAL/inc/nios2.h \
+  ../capstone_bsp//HAL/inc/alt_types.h ../capstone_bsp/system.h \
+  ../capstone_bsp//UCOSII/inc/os_cfg.h \
+  ../capstone_bsp//HAL/inc/sys/alt_alarm.h \
+  ../capstone_bsp//HAL/inc/sys/alt_llist.h \
+  ../capstone_bsp//HAL/inc/priv/alt_alarm.h \
+  ../capstone_bsp//HAL/inc/alt_types.h ../capstone_bsp/system.h \
+  ../capstone_bsp//UCOSII/inc/ucos_ii.h \
+  ../capstone_bsp//UCOSII/inc/os_cfg.h ../capstone_bsp//HAL/inc/os_cpu.h \
+  ../capstone_bsp//iniche/src/h/tk_crnos.h \
+  ../capstone_bsp//iniche/src/h/libport.h \
+  ../capstone_bsp//iniche/src/h/tcpport.h \
+  ../capstone_bsp//iniche/src/h/libport.h \
+  ../capstone_bsp//iniche/src/h/q.h \
+  ../capstone_bsp//iniche/src/h/netbuf.h \
+  ../capstone_bsp//iniche/src/h/net.h \
+  ../capstone_bsp//iniche/src/h/profiler.h \
+  ../capstone_bsp//iniche/src/h/ip.h \
+  ../capstone_bsp//iniche/src/h/nptcp.h \
+  ../capstone_bsp//iniche/src/h/mbuf.h \
+  ../capstone_bsp//iniche/src/h/socket.h \
+  ../capstone_bsp//iniche/src/h/sockvar.h \
+  ../capstone_bsp//iniche/src/h/sockcall.h \
+  ../capstone_bsp//iniche/src/h/bsdsock.h \
+  ../capstone_bsp//iniche/src/h/tcpport.h \
+  ../capstone_bsp//HAL/inc/sys/alt_llist.h dm9000a_regs.h basic_io.h \
+  ../capstone_bsp//HAL/inc/io.h ../capstone_bsp//HAL/inc/alt_types.h \
+  ../capstone_bsp/system.h ../capstone_bsp//HAL/inc/sys/alt_irq.h \
+  ../capstone_bsp//iniche/src/h/ether.h
+
+dm9000a.h:
+
+../capstone_bsp//iniche/inc/alt_iniche_dev.h:
+
+../capstone_bsp//iniche/src/h/nios2/ipport.h:
+
+../capstone_bsp/system.h:
+
+../capstone_bsp/linker.h:
+
+../capstone_bsp//iniche/src/h/nptypes.h:
+
+../capstone_bsp//HAL/inc/includes.h:
+
+../capstone_bsp//HAL/inc/os_cpu.h:
+
+../capstone_bsp//HAL/inc/sys/alt_irq.h:
+
+../capstone_bsp//HAL/inc/nios2.h:
+
+../capstone_bsp//HAL/inc/alt_types.h:
+
+../capstone_bsp/system.h:
+
+../capstone_bsp//UCOSII/inc/os_cfg.h:
+
+../capstone_bsp//HAL/inc/sys/alt_alarm.h:
+
+../capstone_bsp//HAL/inc/sys/alt_llist.h:
+
+../capstone_bsp//HAL/inc/priv/alt_alarm.h:
+
+../capstone_bsp//HAL/inc/alt_types.h:
+
+../capstone_bsp/system.h:
+
+../capstone_bsp//UCOSII/inc/ucos_ii.h:
+
+../capstone_bsp//UCOSII/inc/os_cfg.h:
+
+../capstone_bsp//HAL/inc/os_cpu.h:
+
+../capstone_bsp//iniche/src/h/tk_crnos.h:
+
+../capstone_bsp//iniche/src/h/libport.h:
+
+../capstone_bsp//iniche/src/h/tcpport.h:
+
+../capstone_bsp//iniche/src/h/libport.h:
+
+../capstone_bsp//iniche/src/h/q.h:
+
+../capstone_bsp//iniche/src/h/netbuf.h:
+
+../capstone_bsp//iniche/src/h/net.h:
+
+../capstone_bsp//iniche/src/h/profiler.h:
+
+../capstone_bsp//iniche/src/h/ip.h:
+
+../capstone_bsp//iniche/src/h/nptcp.h:
+
+../capstone_bsp//iniche/src/h/mbuf.h:
+
+../capstone_bsp//iniche/src/h/socket.h:
+
+../capstone_bsp//iniche/src/h/sockvar.h:
+
+../capstone_bsp//iniche/src/h/sockcall.h:
+
+../capstone_bsp//iniche/src/h/bsdsock.h:
+
+../capstone_bsp//iniche/src/h/tcpport.h:
+
+../capstone_bsp//HAL/inc/sys/alt_llist.h:
+
+dm9000a_regs.h:
+
+basic_io.h:
+
+../capstone_bsp//HAL/inc/io.h:
+
+../capstone_bsp//HAL/inc/alt_types.h:
+
+../capstone_bsp/system.h:
+
+../capstone_bsp//HAL/inc/sys/alt_irq.h:
+
+../capstone_bsp//iniche/src/h/ether.h:
