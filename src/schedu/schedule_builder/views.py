d0add006aec227b09d74e2f1e9dd37f380ebe839
edcarter <edcarter@ualberta.ca>
2017-02-04 20:56:04 -0700

    added 404 codes to user and schedule

--- src/schedu/schedule_builder/views.py
+++ src/schedu/schedule_builder/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Image 
+from .models import * 
 # Create your views here.
 
 def index(request):
@@ -11,10 +11,12 @@ def image(request):
     return render(request, 'schedule_builder/image.html')
 
 def user(request, username):
-    return HttpResponse('You are looking at the page for user: {}'.format(username))
+    user = get_object_or_404(User, pk=username)
+    return HttpResponse('You are looking at the page for user: {}'.format(user.name))
 
-def schedule(request, username,  schedule_id):
-    return HttpResponse('You are looking at the schedule for user: {} with id: {}'.format(username, schedule_id))
+def schedule(request, username, schedule_id):
+    schedule = get_object_or_404(Schedule, user__name=username, id=schedule_id)
+    return HttpResponse('You are looking at the schedule for user: {} with id: {}'.format(schedule.user.name, schedule.id))
 
 def home(request):
     return HttpResponse('You are at the home page!')
