991e38a431e5ec7417eb659b537661652cbf9b83
edcarter <edcarter@ualberta.ca>
2017-02-04 17:01:25 -0700

    added configuration for admin site

--- src/schedu/schedule_builder/models.py
+++ src/schedu/schedule_builder/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Image(models.Model):
--- src/schedu/schedule_builder/urls.py
+++ src/schedu/schedule_builder/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
 from . import views
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^image/$', views.image, name='image'),
     url(r'^test-image/$', views.image, name='image'),
 ]
