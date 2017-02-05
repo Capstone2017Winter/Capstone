086b41907869677d77bf99d93ae5f6a25df68b1a
edcarter <edcarter@ualberta.ca>
2017-02-04 20:16:27 -0700

    defined views and urls for users and schedules

--- src/schedu/schedule_builder/urls.py
+++ src/schedu/schedule_builder/urls.py
@@ -7,6 +7,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<username>[a-zA-Z0-9]+)/$', views.user, name='user'),
+    url(r'^(?P<username>[a-zA-Z0-9]+)/(?P<schedule_id>[0-9]+)/$', views.schedule, name='schedule'),
     url(r'^image/$', views.image, name='image'),
     url(r'^test-image/$', views.image, name='image'),
 ]
--- src/schedu/schedule_builder/views.py
+++ src/schedu/schedule_builder/views.py
@@ -10,3 +10,9 @@ def index(request):
 
 def image(request):
     return render(request, 'schedule_builder/image.html')
+
+def user(request, username):
+    return HttpResponse('You are looking at the page for user: {}'.format(username))
+
+def schedule(request, username,  schedule_id):
+    return HttpResponse('You are looking at the schedule for user: {} with id: {}'.format(username, schedule_id))
