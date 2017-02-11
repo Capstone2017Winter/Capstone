5b79171e540e15da0f80ec849d859b10a71b4483
edcarter <edcarter@ualberta.ca>
2017-02-11 14:46:01 -0700

    added constraint that the same class could not be added twice

--- src/schedu/schedule_builder/static/schedule_builder/schedule.js
+++ src/schedu/schedule_builder/static/schedule_builder/schedule.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	classes = new Array();
 	$('#addClassButton').click(function(){
 		var classNameInput = $("#classSearchTextId").val();
+		if (classes.indexOf(classNameInput) != -1) {
+			alert("This class is already in your list");
+			return;
+		}
 		classes.push(classNameInput);
 		$('#classListId').append('<li><p>' + classNameInput + "</p></li>");
 	});
