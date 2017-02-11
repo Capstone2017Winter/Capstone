aed69f4d8872cbda5f89c07d5113b7f019960066
edcarter <edcarter@ualberta.ca>
2017-02-11 14:38:47 -0700

    added class name box and add class button

--- src/schedu/schedule_builder/static/schedule_builder/schedule.js
+++ src/schedu/schedule_builder/static/schedule_builder/schedule.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+	classes = new Array();
 	$('#addClassButton').click(function(){
 		var classNameInput = $("#classSearchTextId").val();
+		classes.push(classNameInput);
 		$('#classListId').append('<li><p>' + classNameInput + "</p></li>");
 	});
 });
