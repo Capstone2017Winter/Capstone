33eacfc7055248c2a96a6aec7235dcdebfd33cec
edcarter <edcarter@ualberta.ca>
2017-02-13 15:03:14 -0700

    change static url to not reference localhost

--- src/schedu/schedu/settings.py
+++ src/schedu/schedu/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = 'http://localhost:80/static/'
+STATIC_URL = '/static/'
 
 #define where we store our media (image) files
 ENV_PATH = os.path.abspath(os.path.dirname(__file__))
