283c617e56536a71d01536ff6872c57b15ffcbbb
Salim1993 <salimbenkhaled93@gmail.com>
2017-03-03 15:47:04 -0700

    New webserver stuff added to master.

---
+++ hardware/niosII_microc_capstone/software/Webserver/obj/default/web_server.d
@@ -0,0 +1,203 @@
+obj/default/web_server.o: web_server.c \
+  ../Webserver_bsp//HAL/inc/includes.h ../Webserver_bsp//HAL/inc/os_cpu.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_irq.h \
+  ../Webserver_bsp//HAL/inc/nios2.h ../Webserver_bsp//HAL/inc/alt_types.h \
+  ../Webserver_bsp/system.h ../Webserver_bsp/linker.h \
+  ../Webserver_bsp//UCOSII/inc/os_cfg.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_alarm.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_llist.h \
+  ../Webserver_bsp//HAL/inc/priv/alt_alarm.h \
+  ../Webserver_bsp//HAL/inc/alt_types.h ../Webserver_bsp/system.h \
+  ../Webserver_bsp//UCOSII/inc/ucos_ii.h \
+  ../Webserver_bsp//UCOSII/inc/os_cfg.h \
+  ../Webserver_bsp//HAL/inc/os_cpu.h ../Webserver_bsp/system.h \
+  alt_error_handler.h web_server.h ../Webserver_bsp//HAL/inc/alt_types.h \
+  ../Webserver_bsp//iniche/src/h/nios2/ipport.h ../Webserver_bsp/system.h \
+  ../Webserver_bsp//iniche/src/h/nptypes.h \
+  ../Webserver_bsp//HAL/inc/includes.h \
+  ../Webserver_bsp//iniche/src/h/tk_crnos.h \
+  ../Webserver_bsp//iniche/src/h/libport.h \
+  ../Webserver_bsp//iniche/src/h/libport.h \
+  ../Webserver_bsp//iniche/src/nios2/osport.h \
+  ../Webserver_bsp//iniche/src/h/nios2/ipport.h \
+  ../Webserver_bsp//HAL/inc/includes.h ../Webserver_bsp//iniche/src/h/q.h \
+  ../Webserver_bsp//iniche/src/h/netbuf.h \
+  ../Webserver_bsp//iniche/src/h/net.h ../Webserver_bsp//iniche/src/h/q.h \
+  ../Webserver_bsp//iniche/src/h/profiler.h \
+  ../Webserver_bsp//iniche/src/h/arp.h \
+  ../Webserver_bsp//iniche/src/h/ip.h \
+  ../Webserver_bsp//iniche/src/h/icmp.h \
+  ../Webserver_bsp//iniche/src/h/udp.h \
+  ../Webserver_bsp//iniche/src/h/nvparms.h \
+  ../Webserver_bsp//iniche/src/h/nios2/ipport.h \
+  ../Webserver_bsp//iniche/src/h/libport.h \
+  ../Webserver_bsp//iniche/src/h/nvfsio.h \
+  ../Webserver_bsp//iniche/src/h/menu.h \
+  ../Webserver_bsp//iniche/src/h/in_utils.h \
+  ../Webserver_bsp//iniche/src/h/app_ping.h \
+  ../Webserver_bsp//iniche/src/h/task.h \
+  ../Webserver_bsp//iniche/src/h/tcpport.h \
+  ../Webserver_bsp//iniche/src/h/netbuf.h \
+  ../Webserver_bsp//iniche/src/h/net.h \
+  ../Webserver_bsp//iniche/src/h/ip.h \
+  ../Webserver_bsp//iniche/src/h/nptcp.h \
+  ../Webserver_bsp//iniche/src/h/mbuf.h \
+  ../Webserver_bsp//iniche/src/h/socket.h \
+  ../Webserver_bsp//iniche/src/h/sockvar.h \
+  ../Webserver_bsp//iniche/src/h/sockcall.h \
+  ../Webserver_bsp//iniche/src/h/bsdsock.h \
+  ../Webserver_bsp//iniche/src/h/tcpport.h \
+  ../Webserver_bsp//iniche/src/h/net.h dm9000a.h \
+  ../Webserver_bsp//iniche/inc/alt_iniche_dev.h \
+  ../Webserver_bsp//iniche/src/h/nios2/ipport.h \
+  ../Webserver_bsp//iniche/src/h/tcpport.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_llist.h \
+  ../Webserver_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_dev.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_llist.h \
+  ../Webserver_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_llist.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_alarm.h \
+  ../Webserver_bsp//HAL/inc/sys/alt_warning.h
+
+../Webserver_bsp//HAL/inc/includes.h:
+
+../Webserver_bsp//HAL/inc/os_cpu.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_irq.h:
+
+../Webserver_bsp//HAL/inc/nios2.h:
+
+../Webserver_bsp//HAL/inc/alt_types.h:
+
+../Webserver_bsp/system.h:
+
+../Webserver_bsp/linker.h:
+
+../Webserver_bsp//UCOSII/inc/os_cfg.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_llist.h:
+
+../Webserver_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Webserver_bsp//HAL/inc/alt_types.h:
+
+../Webserver_bsp/system.h:
+
+../Webserver_bsp//UCOSII/inc/ucos_ii.h:
+
+../Webserver_bsp//UCOSII/inc/os_cfg.h:
+
+../Webserver_bsp//HAL/inc/os_cpu.h:
+
+../Webserver_bsp/system.h:
+
+alt_error_handler.h:
+
+web_server.h:
+
+../Webserver_bsp//HAL/inc/alt_types.h:
+
+../Webserver_bsp//iniche/src/h/nios2/ipport.h:
+
+../Webserver_bsp/system.h:
+
+../Webserver_bsp//iniche/src/h/nptypes.h:
+
+../Webserver_bsp//HAL/inc/includes.h:
+
+../Webserver_bsp//iniche/src/h/tk_crnos.h:
+
+../Webserver_bsp//iniche/src/h/libport.h:
+
+../Webserver_bsp//iniche/src/h/libport.h:
+
+../Webserver_bsp//iniche/src/nios2/osport.h:
+
+../Webserver_bsp//iniche/src/h/nios2/ipport.h:
+
+../Webserver_bsp//HAL/inc/includes.h:
+
+../Webserver_bsp//iniche/src/h/q.h:
+
+../Webserver_bsp//iniche/src/h/netbuf.h:
+
+../Webserver_bsp//iniche/src/h/net.h:
+
+../Webserver_bsp//iniche/src/h/q.h:
+
+../Webserver_bsp//iniche/src/h/profiler.h:
+
+../Webserver_bsp//iniche/src/h/arp.h:
+
+../Webserver_bsp//iniche/src/h/ip.h:
+
+../Webserver_bsp//iniche/src/h/icmp.h:
+
+../Webserver_bsp//iniche/src/h/udp.h:
+
+../Webserver_bsp//iniche/src/h/nvparms.h:
+
+../Webserver_bsp//iniche/src/h/nios2/ipport.h:
+
+../Webserver_bsp//iniche/src/h/libport.h:
+
+../Webserver_bsp//iniche/src/h/nvfsio.h:
+
+../Webserver_bsp//iniche/src/h/menu.h:
+
+../Webserver_bsp//iniche/src/h/in_utils.h:
+
+../Webserver_bsp//iniche/src/h/app_ping.h:
+
+../Webserver_bsp//iniche/src/h/task.h:
+
+../Webserver_bsp//iniche/src/h/tcpport.h:
+
+../Webserver_bsp//iniche/src/h/netbuf.h:
+
+../Webserver_bsp//iniche/src/h/net.h:
+
+../Webserver_bsp//iniche/src/h/ip.h:
+
+../Webserver_bsp//iniche/src/h/nptcp.h:
+
+../Webserver_bsp//iniche/src/h/mbuf.h:
+
+../Webserver_bsp//iniche/src/h/socket.h:
+
+../Webserver_bsp//iniche/src/h/sockvar.h:
+
+../Webserver_bsp//iniche/src/h/sockcall.h:
+
+../Webserver_bsp//iniche/src/h/bsdsock.h:
+
+../Webserver_bsp//iniche/src/h/tcpport.h:
+
+../Webserver_bsp//iniche/src/h/net.h:
+
+dm9000a.h:
+
+../Webserver_bsp//iniche/inc/alt_iniche_dev.h:
+
+../Webserver_bsp//iniche/src/h/nios2/ipport.h:
+
+../Webserver_bsp//iniche/src/h/tcpport.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_llist.h:
+
+../Webserver_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_dev.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_llist.h:
+
+../Webserver_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_llist.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Webserver_bsp//HAL/inc/sys/alt_warning.h:
