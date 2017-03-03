b43d85fa5e66f745322372562de4438933a604d3
Scott Wowk <sdwowk@ualberta.ca>
2017-03-03 14:44:59 -0700

    Vga drawing properly

--- hardware/niosII_microc_capstone/software/vga/hello_ucosii.c
+++ hardware/niosII_microc_capstone/software/vga/hello_ucosii.c
@@ -51,6 +51,8 @@
 #include "includes.h"
 #include "altera_up_sd_card_avalon_interface.h"
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
+//#include <curl/curl.h>
+
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
@@ -67,101 +69,10 @@ OS_STK    task2_stk[TASK_STACKSIZE];
 #define VGA_256_COLOR_MODE  0x13      /* use to set 256-color mode. */
 #define TEXT_MODE           0x03      /* use to set 80x25 text mode. */
 
-#define SCREEN_WIDTH        800       /* width in pixels of mode 0x13 */
+#define SCREEN_WIDTH        640       /* width in pixels of mode 0x13 */
 #define SCREEN_HEIGHT       480       /* height in pixels of mode 0x13 */
 #define NUM_COLORS          256       /* number of colors in mode 0x13 */
 
-typedef unsigned char  byte;
-typedef unsigned short word;
-typedef unsigned long  dword;
-
-byte *VGA = (byte *)0xA0000;          /* this points to video memory. */
-word *my_clock = (word *)0x046C;      /* this points to the 18.2hz system
-                                           clock. */
-
-typedef struct tagBITMAP              /* the structure for a bitmap. */
-{
-  word width;
-  word height;
-  byte *data;
-} BITMAP;
-
-/**************************************************************************
- *  fskip                                                                 *
- *     Skips bytes in a file.                                             *
- **************************************************************************/
-
-void fskip(FILE *fp, int num_bytes)
-{
-   int i;
-   for (i=0; i<num_bytes; i++)
-      fgetc(fp);
-}
-
-/**************************************************************************
- *  load_bmp                                                              *
- *    Loads a bitmap file into memory.                                    *
- **************************************************************************/
-
-void load_bmp(char *file,BITMAP *b)
-{
-  FILE *fp;
-  long index;
-  word num_colors;
-  int x;
-
-
-
-  /* read in the width and height of the image, and the
-     number of colors used; ignore the rest */
-  fskip(fp,16);
-  fread(&b->width, sizeof(word), 1, fp);
-  fskip(fp,2);
-  fread(&b->height,sizeof(word), 1, fp);
-  fskip(fp,22);
-  fread(&num_colors,sizeof(word), 1, fp);
-  fskip(fp,6);
-
-  /* assume we are working with an 8-bit file */
-  if (num_colors==0) num_colors=256;
-
-
-  /* try to allocate memory */
-  if ((b->data = (byte *) malloc((word)(b->width*b->height))) == NULL)
-  {
-    fclose(fp);
-    printf("Error allocating memory for file %s.\n",file);
-    exit(1);
-  }
-
-  /* Ignore the palette information for now.
-     See palette.c for code to read the palette info. */
-  fskip(fp,num_colors*4);
-
-
-
-  fclose(fp);
-}
-
-/**************************************************************************
- *  draw_bitmap                                                           *
- *    Draws a bitmap.                                                     *
- **************************************************************************/
-
-void draw_bitmap(BITMAP *bmp,int x,int y)
-{
-  int j;
-  word screen_offset = (y<<8)+(y<<6)+x;
-  word bitmap_offset = 0;
-
-  for(j=0;j<bmp->height;j++)
-  {
-    memcpy(&VGA[screen_offset],&bmp->data[bitmap_offset],bmp->width);
-
-    bitmap_offset+=bmp->width;
-    screen_offset+=SCREEN_WIDTH;
-  }
-}
 
 
 /* The main function creates two task and starts multi-tasking */
@@ -188,11 +99,11 @@ int main(void)
 
   short int file_attributes = alt_up_sd_card_get_attributes(file_handle);
   printf("File Attributes: %d\n",file_attributes );
-  byte data;
+  unsigned char data;
   printf("Reading Data \n");
 
   int times_read = 0;
-  unsigned char data_array[640*480*4];
+  unsigned char data_array[SCREEN_WIDTH][SCREEN_HEIGHT];
   //BITMAP bmp;
   for(times_read = 0; times_read < 54; times_read++){
 	  data = alt_up_sd_card_read(file_handle);
@@ -210,14 +121,22 @@ int main(void)
   int x, y;
   unsigned char byte_b, byte_g, byte_r;
   /* read the bitmap */
-  for(y=0;y<480;y++){
-	  for(x=0;x<640;x++)  {
-  byte_b=(unsigned char)(alt_up_sd_card_read(file_handle));
-  byte_g=(unsigned char)(alt_up_sd_card_read(file_handle));
-  byte_r=(unsigned char)(alt_up_sd_card_read(file_handle));
-  data_array[x+480*y]=((byte_r>>3)<<11)|((byte_g>>2)<<5)|((byte_b>>3)<<0);
+  for(y = (SCREEN_HEIGHT-1); y >= 0; y--){
+	for(x = 0; x < SCREEN_WIDTH; x++){
+		byte_b= alt_up_sd_card_read(file_handle);
+		byte_g= alt_up_sd_card_read(file_handle);
+		byte_r= alt_up_sd_card_read(file_handle);
+		if((byte_b < 0) || (byte_g < 0) || (byte_r < 0)){
+			break;
+		}
 
-  }
+		byte_b = (unsigned char) byte_b;
+		byte_g = (unsigned char) byte_g;
+		byte_r = (unsigned char) byte_r;
+
+		data_array[x][y]=((byte_r>>3)<<11)|((byte_g>>2)<<5)|((byte_b>>3)<<0);
+		times_read++;
+	}
   }
 /*
   while(data >= 0){
@@ -241,13 +160,13 @@ int main(void)
   alt_up_pixel_buffer_dma_clear_screen(vga_buffer, 0);
 
   while(1){
-  for(y = 0; y < 480; y++){
-	  for(x = 0; x < 640; x++){
-		  alt_up_pixel_buffer_dma_draw(vga_buffer, data_array[x + 480*y],x,y);
+	  for(y = 0; y < SCREEN_HEIGHT; y++){
+		  for(x = 0; x < SCREEN_WIDTH; x++){
+			  alt_up_pixel_buffer_dma_draw(vga_buffer, data_array[x][y],x,y);
 
 
+		  }
 	  }
-  }
   //printf("Finished displaying data\n");
   }
 }
@@ -255,96 +174,6 @@ int main(void)
 
 
 
-
-  /**************************************************************************
-   *  draw_transparent_bitmap                                               *
-   *    Draws a transparent bitmap.                                         *
-   **************************************************************************/
-/*
-  void draw_transparent_bitmap(BITMAP *bmp,int x,int y)
-  {
-    int i,j;
-    word screen_offset = (y<<8)+(y<<6);
-    word bitmap_offset = 0;
-    byte data;
-
-    for(j=0;j<bmp->height;j++)
-    {
-      for(i=0;i<bmp->width;i++,bitmap_offset++)
-      {
-        data = bmp->data[bitmap_offset];
-        if (data) VGA[screen_offset+x+i] = data;
-      }
-      screen_offset+=SCREEN_WIDTH;
-    }
-  }
-*/
-  /**************************************************************************
-   *  wait                                                                  *
-   *    Wait for a specified number of clock ticks (18hz).                  *
-   **************************************************************************/
-
-
-
-  /**************************************************************************
-   *  Main                                                                  *
-   *    Draws opaque and transparent bitmaps                                *
-   **************************************************************************/
-/*
-  void main()
-  {
-    int i,x,y;
-    BITMAP bmp;
-
-    if (__djgpp_nearptr_enable() == 0)
-    {
-      printf("Could get access to first 640K of memory.\n");
-      exit(-1);
-    }
-
-    VGA+=__djgpp_conventional_base;
-    my_clock = (void *)my_clock + __djgpp_conventional_base;
-
-    load_bmp("rocket.bmp",&bmp);        /* open the file */
-/*
-    set_mode(VGA_256_COLOR_MODE);       /* set the video mode. */
-
-    /* draw the background */
-    /*
-    for(i=0;i<200;i++)
-      memset(&VGA[320*i],i,SCREEN_WIDTH);
-
-    wait(25);
-
-    /* draw a tiled bitmap pattern on the left */
-    /*
-  	for(y=0;y<=SCREEN_HEIGHT-bmp.height;y+=bmp.height)
-      for(x=0;x<=(SCREEN_WIDTH)/2-bmp.width;x+=bmp.width)
-        draw_bitmap(&bmp,x,y);
-
-    wait(25);
-
-    /* draw a tiled transparent bitmap pattern on the right */
-    /*
-  	for(y=0;y<=SCREEN_HEIGHT-bmp.height;y+=bmp.height)
-      for(x=SCREEN_WIDTH-bmp.width;x>=SCREEN_WIDTH/2;x-=bmp.width)
-        draw_transparent_bitmap(&bmp,x,y);
-
-    wait(100);
-
-    free(bmp.data);                     /* free up memory used */
-  	/*
-    set_mode(TEXT_MODE);                /* set the video mode back to
-                                           text mode. */
-  /*
-  __djgpp_nearptr_disable();
-
-    return;
-
-  //OSStart();
-  return 0;
-}
-*/
 /******************************************************************************
 *                                                                             *
 * License Agreement                                                           *
