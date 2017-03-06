6a801bd935ef8e3226ee65594a2a82867fe97b1c
edcarter <edcarter@ualberta.ca>
2017-03-05 21:25:56 -0700

    write test for User.is_valid_name()

--- src/schedu/schedule_builder/tests.py
+++ src/schedu/schedule_builder/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+class UserMethodTests(TestCase):
+
+	def test_is_valid_name(self):
+		"""
+		is_valid_name() should return true if the name consists
+		of only alphanumeric characters, and false otherwise
+		"""
+		self.assertIs(bool(User.is_valid_name('edcarter')), True)
+		self.assertIs(bool(User.is_valid_name('edcarter94')), True)
+		self.assertIs(bool(User.is_valid_name('')), False)
+		self.assertIs(bool(User.is_valid_name(' ')), False)
+		self.assertIs(bool(User.is_valid_name('e carter')), False)
+		self.assertIs(bool(User.is_valid_name('e_carter')), False)
+		self.assertIs(bool(User.is_valid_name('e.carter')), False)
