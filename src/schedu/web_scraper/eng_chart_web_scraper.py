74002640311ffadf0d394ff272c036a401d83f1c
Scott <sdwowk@ualberta.ca>
2017-03-21 20:03:52 -0600

    Have all table data in a text file so as to be easily read for eventual mapping to python objects

--- src/schedu/web_scraper/eng_chart_web_scraper.py
+++ src/schedu/web_scraper/eng_chart_web_scraper.py
@@ -2,4 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("https://www.registrar.ualberta.ca/calendar-archive/calendar-2015-2016/Undergrad/Engineering/chart1.html")
 bsObj = BeautifulSoup(html.read())
-print(bsObj.div.table)
+tables = bsObj.findAll("table")
+tableData = open("TableData.txt",'w')
+filter(None,tables)
+
+for line in tables:
+	if(line is not None):
+		tableData.write(str(line))
+
+tableData.close()
